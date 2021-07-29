f8d6f5de073ad9e50c5e8038135bc4bed6ce318a
wliu <wliu0709@gmail.com>
2021-07-28 21:25:22 -0400

    Update menu

--- src/scripts/drop_down.js
+++ src/scripts/drop_down.js
@@ -4,25 +4,26 @@ export default result =>{
     let key;
     const xUl = document.getElementById('x-ul')
     Object.keys(result).forEach(key=>{
-    const xLi = document.createElement('li')
-    xLi.classList.add('x-content')
-    xLi.innerHTML = key
-    xUl.appendChild(xLi)
-    xLi.addEventListener('click', e=>{
-            if(!e.target.classList.contains('active')){
-                Array.from(xUl.getElementsByClassName('active')).forEach(li=>{
-                    li.classList.remove('active');
-                })
-                e.target.classList.add('active')
-                key=e.target.innerHTML
-                console.log(key)
-                loadKey(key)
-            }
-        })
-    const yUl = document.getElementById('y-ul')
-    // console.log(result[key][0])
-    // console.log(Object.keys(result[key][0]).slice(1))
-    Object.keys(result[key][0]).slice(1).forEach(YKey=>{
+        const xLi = document.createElement('li')
+        xLi.classList.add('x-content')
+        xLi.innerHTML = key
+        xUl.appendChild(xLi)
+        xLi.addEventListener('click', e=>{
+                if(!e.target.classList.contains('active')){
+                    Array.from(xUl.getElementsByClassName('active')).forEach(li=>{
+                        li.classList.remove('active');
+                    })
+                    e.target.classList.add('active')
+                    key=e.target.innerHTML
+                    console.log(key)
+                    loadKey(key)
+                }
+            })
+        // console.log(result[key][0])
+        // console.log(Object.keys(result[key][0]).slice(1))
+    })
+         const yUl = document.getElementById('y-ul')
+   Object.keys(result['Borough'][0]).slice(1).forEach(YKey=>{
         
         const yLi = document.createElement('li')
         yLi.classList.add('y-content')
@@ -39,7 +40,6 @@ export default result =>{
                 // loadKey(key)
             }
         })
-    })
     })
 
 
