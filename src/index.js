bc0fc35d22be07da81cd38e30bfa0b72a6c8a015
wliu <wliu0709@gmail.com>
2021-07-30 12:38:02 -0400

    modal work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import data_parse from './scripts/data_parse'
 import drop_down from './scripts/drop_down'
 import * as d3 from 'd3';
 
+//modal intro page
+    document.addEventListener('DOMContentLoaded',()=>{
+    let modal = document.getElementById('modal');
+    let modalcontainer = document.getElementById('modal-container')
+         modal.style.display='block'
+    let btn = document.getElementById('modal-btn')
+    btn.addEventListener('click', e=>{
+        modalcontainer.style.display='none'
+        modal.style.display='none'
+   
+    })
+})
+
+
 const dataSources ={
     'NYC Hiking': '../data/NYC_Hiking.json'
 }
@@ -10,9 +24,8 @@ const source = dataSources[Object.keys(dataSources)[0]]
 let result
 
 d3.json(source).then(data=>{
-    //data is rawdata here
+
     result = data_parse(data)
-    console.log(result)
     drop_down(result)
     loadKey("Borough", 'TotalNumber');
 })
@@ -20,6 +33,5 @@ d3.json(source).then(data=>{
 export const loadKey = (key='Borough', subkey='TotalNumber') =>{
     data_bind(result[key], subkey);
    
-    // console.log('loadkey')
 }
 
---
+++ src/scripts/modal.js
@@ -0,0 +1,4 @@
+// document.addEventListener('DOMContentLoaded',()=>{
+//     moadal = document.getElementById('modal-container')
+//     modal.style.display='block'
+// })
